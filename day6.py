ab67adec41050935aa4cc1e31e9717966dc2a5cf
daredevil-grbll <crazyzacke@gmail.com>
2021-12-05 23:27:51 -0800

    Linear algebra solution

--- day6.py
+++ day6.py
@@ -1,3 +1,6 @@
+import numpy as np
+from numpy.linalg import matrix_power
+
 def solve(data, days = 81):
     fishes = {i:0 for i in range(-1, 9)}
     for d in data:
@@ -14,7 +17,34 @@ def solve(data, days = 81):
     [a := a + v for v in fishes.values()]
     print(a)
 
+# A**n * x = y
+# x[0] -> y[6]
+# x[0] -> y[8]
+def linalg_solution(data, days= 81):
+    fishes = np.array([0 for i in range(9)])
+    for i in data:
+        fishes[i] += 1
+
+    A = matrix_power(np.array([
+    #   [0 1 2 3 4 5 6 7 8]
+        [0,1,0,0,0,0,0,0,0], #[0]
+        [0,0,1,0,0,0,0,0,0], #[1]
+        [0,0,0,1,0,0,0,0,0], #[2]
+        [0,0,0,0,1,0,0,0,0], #[3]
+        [0,0,0,0,0,1,0,0,0], #[4]
+        [0,0,0,0,0,0,1,0,0], #[5]
+        [1,0,0,0,0,0,0,1,0], #[6]
+        [0,0,0,0,0,0,0,0,1], #[7]
+        [1,0,0,0,0,0,0,0,0], #[8]
+    ], dtype=np.longlong), days)
+
+    fishes =  A * fishes
+
+    print(fishes.sum())
+
 if __name__ == '__main__':
     data = list(map(int, open('input6_1.txt', 'r').read().split(',')))
-    solve(data[:])
-    solve(data[:], days=257)
+    linalg_solution(data, days = 80)
+    linalg_solution(data, days = 256)
+    #solve(data[:])
+    #solve(data[:], days=257)
